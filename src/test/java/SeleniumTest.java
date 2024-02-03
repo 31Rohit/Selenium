65a458ea6eb44b452c52c2e7320e77ecfe87865f
31Rohit <92738257+31Rohit@users.noreply.github.com>
2024-02-03 14:14:35 +0530

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -6,13 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class SeleniumTest {
- @Test
-   static void Setup(){
-     System.setProperty("webDriver.chrome.driver","src/test/resources/chromedriver.exe");
-     WebDriver driver=new ChromeDriver();
-     driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407");
-     JavascriptExecutor js = (JavascriptExecutor)driver;
-     js.executeScript("scrollBy(0,1000)");
+ // ----------Automation using method---------------------
+ // @Test
+   // static void Setup(){
+   //   System.setProperty("webDriver.chrome.driver","src/test/resources/chromedriver.exe");
+   //   WebDriver driver=new ChromeDriver();
+   //  WebElement firstName= driver.findElement(By.id("RESULT_TextField-1"));
+   //     firstName.sendKeys("FirstName");
+   //     driver.findElement(By.name("RESULT_TextField-2")).sendKeys("LastName");
+   //     WebElement phoneNumber=driver.findElement(By.id("RESULT_TextField-3"));
+   //      if (phoneNumber.isDisplayed() && phoneNumber.isEnabled()) {
+
+   //          phoneNumber.sendKeys("1234567890");}
+   //      else
+   //          System.out.println("Phone number field is not visible or enabled.");
+   //     driver.findElement(By.xpath("//input[@value=Submit]")).click();
+     // driver.get("https://fs2.formsite.com/meherpavan/form2/index.html?1537702596407");
+     // JavascriptExecutor js = (JavascriptExecutor)driver;
+     // js.executeScript("scrollBy(0,1000)");
  }
 
     public static void main(String[] args) {
